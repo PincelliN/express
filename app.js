31ba7ebf548c661151e70d41a46ffec147b11afc
Nicolò Pincelli <pincelli.n@gmail.com>
2025-07-09 18:00:19 +0200

    feat : add middleware

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const express = require("express");
 
 const app = express();
 
+// utile per intercettare tutte le richieste diverse da / /add-product
+app.use("/", (req, res, next) => {
+  console.log("this always run!");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware!2");
+  res.send("<h1> The 'Add-product' Page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1>Hello From Express</h1>");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
