5e4c27409a2f63da2ded7b5cc8bd144e3a107159
Nicolò Pincelli <pincelli.n@gmail.com>
2025-07-14 12:47:05 +0200

    feat: page  not found

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const http = require("http");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRouter);
+app.use(shopRouter);
 // utile per intercettare tutte le richieste diverse da / /add-product
+/*
 app.use("/", (req, res, next) => {
-  console.log("this always run!");
   next();
 });
-
-app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware!2");
-  res.send("<h1> The 'Add-product' Page</h1>");
-});
-
-app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Hello From Express</h1>");
+*/
+app.use((req, res, next) => {
+  res.status(404).send("<h1> Page not Found </h1>");
 });
 
 const server = http.createServer(app);
